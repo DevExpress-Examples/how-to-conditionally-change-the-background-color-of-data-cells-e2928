7457a5c1cca56c4c26853950af5b7ba74bbf06e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ChangeRowAppearance/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using DevExpress.Xpf.Core.WPFCompatibility;
+using DevExpress.Xpf.Grid;
+using System.Windows.Markup;
+
+namespace ChangeRowAppearance {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+        }
+    }
+
+    public class ValToClrConverter : MarkupExtension, IValueConverter {
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            if ((int)value  < 10)
+                return new SolidColorBrush(Colors.Red);
+            if ((int)value > 100)
+                return new SolidColorBrush(Colors.Green);
+            return new SolidColorBrush(Colors.LightGray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Conditionally Change the Background Color of Data Cells
+
+
+<p>This example shows how to conditionally change a column's display template. In this example, the background color of the <strong>Product Name</strong> column depends on the product quantity. For instance, if the <strong>Quantity</strong> is less than 10, the product's name is painted in Red.</p>
+
+<br/>
+
+
